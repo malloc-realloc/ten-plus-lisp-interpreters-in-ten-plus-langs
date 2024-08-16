0e9b01d213d4899167152ab927da070680c7d936
3yqq <malloc_realloc_calloc@outlook.com>
2024-08-16 23:45:59 +0800

    clean code

--- typescript/repl.ts
+++ typescript/repl.ts
@@ -1,8 +1,7 @@
-// src/repl.ts
-import { tokenize } from "./token"; // 假设 tokenize 函数在 token.ts 中定义
-import { parseExpr } from "./parser"; // 假设 parseExpr 函数在 parser.ts 中定义
-import { evalExpr } from "./eval"; // 假设 evalExpr 函数在 eval.ts 中定义
-import { Env } from "./env"; // 假设 Env 类在 env.ts 中定义
+import { tokenize } from "./token";
+import { parseExpr } from "./parser";
+import { evalExpr } from "./eval";
+import { Env } from "./env";
 import * as readline from "readline";
 
 function repl(): void {
