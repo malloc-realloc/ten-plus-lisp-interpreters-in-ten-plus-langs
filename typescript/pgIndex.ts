c06a0fc7c035f7dcf95c2c4d8126e8a6e44325ec
3yqq <malloc_realloc_calloc@outlook.com>
2024-09-10 16:30:24 +0800

    better pgIndex.ts

--- typescript/pgIndex.ts
+++ typescript/pgIndex.ts
@@ -4,33 +4,63 @@ import { evalExpr, evalExprs, evalStrExprs } from "./eval";
 import { parseExprs } from "./parser";
 import { tokenize } from "./token";
 
+type MonitorType = [string, string[], string][];
+
 let text = "";
 
-function userInit(
-  env: Env,
-  expr: string,
-  monitor: Map<string, [string[], string]>
-) {
-  evalStrExprs(env, expr);
+let monitor: [string, string[], string][] = [];
 
-  for (let [key, value] of monitor) {
-    env.set(key, evalStrExprs(env, value[1]));
-  }
+function userInit(env: Env, expr: string, monitor: MonitorType) {
+  evalStrExprs(env, expr);
 }
 
 function userChangeMonitorAndRerun(
   env: Env,
   varName: string,
-  value: [string[], string],
-  monitor: Map<string, [string[], string]>
+  listenToVars: string[],
+  expr: string,
+  monitor: MonitorType
 ) {
-  env.set(varName, evalStrExprs(env, value[1]));
-  for (let [key, value] of monitor) {
-    if (key !== varName) {
-      if (value[0].includes(varName)) {
-        env.set(key, evalStrExprs(env, value[1]));
-      }
+  let isNewItem = true;
+  env.set(varName, evalStrExprs(env, expr));
+  for (let i = 0; i < monitor.length; i++) {
+    if (monitor[i][0] === varName) {
+      isNewItem = false;
+      monitor[i][1] = listenToVars;
+      monitor[i][2] = expr;
     }
+
+    if (monitor[i][1].includes(varName)) {
+      env.set(varName, evalStrExprs(env, monitor[i][2]));
+    }
+  }
+  if (isNewItem) {
+    monitor.push([varName, listenToVars, expr]);
+  }
+}
+
+function userChangeMonitor(
+  varName: string,
+  listenToVars: string[],
+  expr: string,
+  monitor: MonitorType
+) {
+  let isNewItem = true;
+  for (let i = 0; i < monitor.length; i++) {
+    if (monitor[i][0] === varName) {
+      isNewItem = false;
+      monitor[i][1] = listenToVars;
+      monitor[i][2] = expr;
+    }
+  }
+  if (isNewItem) {
+    monitor.push([varName, listenToVars, expr]);
+  }
+}
+
+function rerunMonitor(env: Env, monitor: MonitorType) {
+  for (let i = 0; i < monitor.length; i++) {
+    env.set(monitor[i][0], evalStrExprs(env, monitor[i][2]));
   }
 }
 
@@ -43,26 +73,38 @@ function pgIndex() {
 
   text = "A: Today a Tesla stock price is {a}. {func1}\n{func2}";
 
-  let monitor: Map<string, [string[], string]> = new Map<
-    string,
-    [string[], string]
-  >();
+  userInit(env, "(define taxRate 10)", monitor);
 
-  monitor.set("a", [[], "20000"]);
-  monitor.set("func1", [
+  userChangeMonitor("a", [], "20000", monitor);
+  userChangeMonitor(
+    "func1",
     ["a"],
     '(define taxedPrice (* a taxRate))(if (< taxedPrice 40000) (+ "taxedPriced " taxedPrice " So cheap, we should buy it.") (+ "taxedPriced " taxedPrice " So expensive, you should not buy it"))',
-  ]);
-  monitor.set("func2", [
+    monitor
+  );
+  userChangeMonitor(
+    "func2",
     ["func1", "a"],
     '(+ "B: Your judgement \'" func1 "\' is terrible. Reconsider the price " a " again.")',
-  ]);
+    monitor
+  );
+
+  rerunMonitor(env, monitor);
 
   userInit(env, "(define taxRate 2)", monitor);
 
   console.log(displayedText(env));
 
-  userChangeMonitorAndRerun(env, "a", [[], "10000"], monitor);
+  userChangeMonitorAndRerun(env, "a", [], "10000", monitor);
+
+  console.log(displayedText(env));
+
+  userInit(env, "(define taxRate 2)", monitor);
+
+  text =
+    "A: now taxRate is {taxRate} and Nvidia stock price is {a}. {func1}\n{func2}";
+
+  rerunMonitor(env, monitor);
 
   console.log(displayedText(env));
 }
