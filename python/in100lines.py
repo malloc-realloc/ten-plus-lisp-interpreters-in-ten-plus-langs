92c9876542f51e5bb9c0bcc6f3bd22975124cea3
malloc-realloc <malloc_realloc_calloc@outlook.com>
2024-10-21 18:34:49 +0800

    new in100lines

--- python/in100lines.py
+++ python/in100lines.py
@@ -1,237 +1,117 @@
-from copy import deepcopy
 import operator
-from curses.ascii import isspace
+from functools import reduce
 
 
-class Obj:
-    def __init__(self, type, value) -> None:
-        self.type = type
-        self.value = value
+class Env(dict):
+    def __init__(self, parms=(), args=(), outer=None):
+        self.update(zip(parms, args))
+        self.outer = outer
+
+    def find(self, var):
+        return self if var in self else self.outer.find(var)
 
 
-class Env:
-    def __init__(self, father=None) -> None:
-        self.value = {}
-        self.father = father
-        self.end = False
-
-    def inEnv(self, tok: str):
-        if tok in self.value:
-            return True
-        elif self.father is not None:
-            return self.father.inEnv(tok)
-        else:
-            return False
-
-    def get(self, tok: str):
-        if tok in self.value:
-            return self.value[tok]
-        elif self.father is not None:
-            return self.father.get(tok)
-        else:
-            raise KeyError(f"Token '{tok}' not found in environment or its ancestors")
-
-    def set(self, tok: str, obj: Obj):
-        self.value[tok] = obj
-        return obj
+def parse(program):
+    return read(program.replace("(", " ( ").replace(")", " ) ").split())
 
 
-def getNextWord(i: int, expr: str) -> tuple[int, str]:
-    while i < len(expr) and isspace(expr[i]):
-        i += 1
-    if i < len(expr) and (expr[i] in ["(", ")", '"']):
-        out = expr[i]
-        i += 1
-        return i, out
-    s = ""
-    while (
-        i < len(expr)
-        and not isspace(expr[i])
-        and not (expr[i] == "(" or expr[i] == ")")
-    ):
-        s += expr[i]
-        i += 1
-    return i, s
-
-
-def skipExpr(i, expr) -> tuple[int, str]:
-    _, tok = getNextWord(i, expr)
-    if tok != "(":
-        i, out = getNextWord(i, expr)
-        return i, out
+def read(tokens):
+    if len(tokens) == 0:
+        raise SyntaxError("unexpected EOF")
+    token = tokens.pop(0)
+    if token == "(":
+        L = []
+        while tokens[0] != ")":
+            L.append(read(tokens))
+        tokens.pop(0)  # pop off ')'
+        return L
+    elif token == ")":
+        raise SyntaxError("unexpected )")
     else:
-        l_minus_r = 1
-        i, out = getNextWord(i, expr)
-        while l_minus_r != 0:
-            i, out = getNextWord(i, expr)
-            if out == "(":
-                l_minus_r += 1
-            elif out == ")":
-                l_minus_r -= 1
-        return i, out
+        try:
+            return int(token)
+        except ValueError:
+            try:
+                return float(token)
+            except ValueError:
+                return str(token)
 
 
-addSubMulDiv = {
-    "+": operator.add,
-    "-": operator.sub,
-    "*": operator.mul,
-    "/": operator.truediv,
-}
+def evaluate(x, env):
+    if isinstance(x, str):  # variable reference
+        return env.find(x)[x]
+    elif not isinstance(x, list):  # constant literal
+        return x
+    op, *args = x
+    if op == "quote":  # quotation
+        return args[0]
+    elif op == "if":  # conditional
+        (test, conseq, alt) = args
+        exp = conseq if evaluate(test, env) else alt
+        return evaluate(exp, env)
+    elif op == "define":  # definition
+        (symbol, exp) = args
+        env[symbol] = evaluate(exp, env)
+    elif op == "lambda":  # procedure
+        (parms, body) = args
+        return lambda *args: evaluate(body, Env(parms, args, env))
+    else:  # procedure call
+        proc = evaluate(op, env)
+        vals = [evaluate(arg, env) for arg in args]
+        return proc(*vals)
 
 
-def ifFunc(env, i: int, expr: str) -> tuple[int, Obj]:
-    i, cond = evalExpr(env, i, expr)
-    if cond.value:
-        i, out = evalExpr(env, i, expr)
-        _, nextTok = getNextWord(i, expr)
-        if nextTok == ")":
-            return i, out
-        i, _ = skipExpr(i, expr)
-        return i, out
-    else:
-        i, _ = skipExpr(i, expr)
-        i, out = evalExpr(env, i, expr)
-        return i, out
-
-
-def defineFunc(env, i: int, expr: str) -> tuple[int, Obj]:
-    i, tok = getNextWord(i, expr)
-    i, out = evalExpr(env, i, expr)
-    env.set(tok, out)
-    return i, out
-
-
-def returnLambdaObjFunc(env, i: int, expr: str) -> tuple[int, Obj]:
-    i, tok = getNextWord(i, expr)  # skip "("
-    params = []
-    while tok != ")":
-        i, tok = getNextWord(i, expr)
-        if tok == ",":
-            continue
-        params.append(tok)
-    params.pop()  # remove ")"
-
-    start = i
-    j = i
-    prev = i
-    _, tok = getNextWord(j, expr)
-    while tok != ")":
-        j, _ = skipExpr(j, expr)
-        _, tok = getNextWord(j, expr)
-    i = j
-
-    out = Obj("lambda", {"expr": expr[start:j].strip(), "vars": params})
-    return i, out
-
-
-def returnFunc(env, i: int, expr: str) -> tuple[int, Obj]:
-    env.end = True
-    i, out = evalExpr(env, i, expr)
-    return i, out
-
-
-builtins = {
-    "if": ifFunc,
-    "define": defineFunc,
-    "lambda": returnLambdaObjFunc,
-    "return": returnFunc,
-}
-
-
-def evalExpr(env, i: int, expr: str) -> tuple[int, Obj]:
-    i, tok = getNextWord(i, expr)
-    if tok == "(":
-        i, out = evalExpr(env, i, expr)
-        i, tok = getNextWord(i, expr)  # skip ")"
-        return i, out
-    if tok.isdigit():
-        return i, Obj("float", float(tok))
-    if tok == '"':
-        start = i
-        for j in range(i, len(expr)):
-            if expr[j] == '"':
-                i = j + 1
-                break
-        return i, Obj("string", expr[start : i - 1])
-
-    if tok in addSubMulDiv:
-        opt = addSubMulDiv[tok]
-        numberObjs = []
-        while tok != ")":
-            i, out = evalExpr(env, i, expr)
-            numberObjs.append(out)
-            _, tok = getNextWord(i, expr)
-        out = numberObjs[0].value
-        for n in numberObjs[1:]:
-            out = opt(out, n.value)
-        return i, Obj("float", out)
-    if tok in builtins:
-        return builtins[tok](env, i, expr)
-    if env.inEnv(tok):
-        if env.get(tok).type != "lambda":
-            return i, env.get(tok)
-        elif env.get(tok).type == "lambda":
-            lambdaFunc = env.get(tok).value  #  {"expr": expr[start:j], "vars": params}
-            params = []
-            while tok != ")":
-                i, out = evalExpr(env, i, expr)
-                params.append(out)
-                _, tok = skipExpr(i, expr)
-
-            # Save the original values of the variables we're about to override
-            newEnv = Env(env)
-            for j, param_name in enumerate(lambdaFunc["vars"]):
-                newEnv.set(param_name, params[j])
-
-            j = 0
-            while j < len(lambdaFunc["expr"]):
-                j, out = evalExpr(newEnv, j, lambdaFunc["expr"])
-                if newEnv.end:
-                    break
-
-            return i, out
-
-    raise ValueError(f"Undefined symbol: {tok}")
-
-
-def repl(env):
+def repl(prompt="pyLisp> "):
+    global_env = Env()
+    global_env.update(
+        {
+            "+": lambda *args: reduce(operator.add, args),
+            "-": lambda *args: reduce(operator.sub, args),
+            "*": lambda *args: reduce(operator.mul, args),
+            "/": lambda *args: reduce(operator.truediv, args),
+            ">": operator.gt,
+            "<": operator.lt,
+            ">=": operator.ge,
+            "<=": operator.le,
+            "=": operator.eq,
+            "abs": abs,
+            "append": operator.add,
+            "apply": lambda proc, args: proc(*args),
+            "begin": lambda *x: x[-1],
+            "car": lambda x: x[0],
+            "cdr": lambda x: x[1:],
+            "cons": lambda x, y: [x] + y,
+            "eq?": operator.is_,
+            "equal?": operator.eq,
+            "length": len,
+            "list": lambda *x: list(x),
+            "list?": lambda x: isinstance(x, list),
+            "map": map,
+            "max": max,
+            "min": min,
+            "not": operator.not_,
+            "null?": lambda x: x == [],
+            "number?": lambda x: isinstance(x, (int, float)),
+            "procedure?": callable,
+            "round": round,
+            "symbol?": lambda x: isinstance(x, str),
+        }
+    )
     while True:
         try:
-            expr: str = input("pyLisp> ").strip()
-            i = 0
-            while i < len(expr):
-                i, out = evalExpr(env, i, expr)
-                print(out.value)
-            if env.end:
-                break
+            val = evaluate(parse(input(prompt)), global_env)
+            if val is not None:
+                print(lisp_str(val))
         except Exception as e:
-            print(f"Error: {e}")
+            print(f"{type(e).__name__}: {e}")
 
 
-def test():
-    env = Env()
-    expr = "(+ 1 2)"
-
-    # First, evaluate the initial expression
-    i = 0
-    while i < len(expr):
-        i, out = evalExpr(env, i, expr)
-        print(out.value)
-
-    # Then start the REPL
-    while True:
-        try:
-            expr: str = input("pyLisp> ").strip()
-            i = 0
-            while i < len(expr):
-                i, out = evalExpr(env, i, expr)
-                print(out.value)
-            if env.end:
-                break
-        except Exception as e:
-            print(f"Error: {e}")
+def lisp_str(exp):
+    if isinstance(exp, list):
+        return "(" + " ".join(map(lisp_str, exp)) + ")"
+    else:
+        return str(exp)
 
 
 if __name__ == "__main__":
-    # repl(Env())
-    test()
+    repl()
